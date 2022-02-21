0f4c5ddcb52f1a3fe80b39b59f223a3ee6928235
wangbin <wangbin@deepblueai.com>
2022-02-21 15:47:00 +0800

    添加组件新css

--- wb-ui.js
+++ wb-ui.js
@@ -1,3 +1,4 @@
+// 等待效果
 let wbLoading = document.querySelector('.wb-loading');
 if (wbLoading) {
   for(let wbloadingI = 0; wbloadingI < 5; wbloadingI ++) {
@@ -6,11 +7,69 @@ if (wbLoading) {
     wbLoading.append(wbloadingDiv);
   }
 }
+// 3d-loading
+let wb3Dloading = document.querySelector(".wb-3d-loading");
+if (wb3Dloading) {
+  let letters = wb3Dloading.textContent.split("");
+  wb3Dloading.textContent = "";
+  letters.forEach((letter, i) => {
+    let span = document.createElement("span");
+    span.textContent = letter;
+    span.style.animationDelay = `${i / 10}s`;
+    wb3Dloading.append(span);
+  });
+}
+// 雪降落效果
 let wbSnow = document.querySelector('.wb-snow');
 if (wbSnow) {
-  for(let wbSnowI = 0; wbSnowI < 98; wbSnowI ++) {
+  for(let wbSnowI = 0; wbSnowI < 90; wbSnowI ++) {
     let wbSnowDiv = document.createElement("div");
     wbSnowDiv.setAttribute('class', 'snow');
     wbSnow.append(wbSnowDiv);
   }
 }
+// 文字效果
+let wblandInTexts = document.querySelectorAll(".wb-landIn");
+if (wblandInTexts) {
+  wblandInTexts.forEach(landInText => {
+    let letters = landInText.textContent.split("");
+    landInText.textContent = "";
+    letters.forEach((letter, i) => {
+      let span = document.createElement("span");
+      span.textContent = letter;
+      span.style.animationDelay = `${i * 0.05}s`;
+      landInText.append(span);
+    });
+  });
+}
+// menu
+let wbMenuLinks = document.querySelectorAll(".wb-menu li a");
+if (wbMenuLinks) {
+  wbMenuLinks.forEach(link => {
+    let letters = link.textContent.split("");
+    link.textContent = "";
+    letters.forEach((letter, i) => {
+      let span = document.createElement("span");
+      span.textContent = letter;
+      span.style.transitionDelay = `${i / 20}s`;
+      span.dataset.text = letter;
+      link.append(span);
+    });
+  });
+}
+// nav
+let navtab = document.querySelector("nav.wb-navtab");
+let navtabItems = document.querySelectorAll("li.wb-navtab-item");
+if (navtab && navtabItems) {
+  navtabItems.forEach((navtabItem, activeIndex) =>
+    navtabItem.addEventListener("click", () => {
+      navtabItems.forEach(navtabItem => navtabItem.classList.remove("active"));
+      navtabItem.classList.add("active");
+      navtab.style.setProperty(
+        "--active-index",
+        `${activeIndex}`
+      );
+    })
+  );
+}
+
